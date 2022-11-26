453bdb6d3523bf0cca406ec8d075f768f2a5eb53
uhanii <74964996+uhanii@users.noreply.github.com>
2022-11-26 22:14:46 +0530

    Null Object Pattern

---
+++ Null Object Pattern.js
@@ -0,0 +1,51 @@
+class User {
+    constructor(id, name) {
+        this.id = id,
+        this.name = name
+    }
+
+    hasAccess() {
+        return this.name === 'Bob';
+    }
+}
+
+class NullUser {
+    constructor() {
+        this.id = -1;
+        this.name = "Guest";
+    }
+    hasAccess() {
+        return false;
+    }
+}
+
+const users = [
+    new User(1, 'Bob'),
+    new User(2, 'John')
+];
+
+function getUser(id) {
+    const user = users.find(user => user.id === id);
+    if(user == null) {
+        return new NullUser();
+    } else {
+        return user;
+    }
+}
+
+function printUser(id) {
+    const user = getUser(id);
+
+    // let name = 'Guest';
+    // if(user != null && user.name != null)
+    //     name = user.name;
+
+    console.log('Hello ' + user.name);
+
+    // if(user != null && user.hasAccess != null && user.hasAccess()) {
+    if(user.hasAccess()) {
+        console.log('You have access');
+    } else {
+        console.log('You are not allowed here');
+    }
+}
